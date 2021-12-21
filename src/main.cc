f77ae38b2eee6aeb263afcfb659a83149acc9ef6
atom9393 <%>
2021-12-21 20:40:29 +0900

    wip

--- src/main.cc
+++ src/main.cc
@@ -10,6 +10,12 @@ inline void view_token(Token* tok) {
 }
 
 int main(int argc, char** argv) {
-  
-
+  try {
+    return Driver().main(argc, argv);
+  }
+  catch( std::exception const& ex ) {
+    std::cout
+      << "unhandled exception has occurred, please report to developer." << std::endl
+      << "what(): " << ex.what() << std::endl;
+  }
 }
