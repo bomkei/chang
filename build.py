039f7f762cc8a62c9d0f6eb1436e43e8bbb0f33c
atom9393 <%>
2021-12-30 09:47:18 +0900

    [buildpy] don't copy sources

--- build.py
+++ build.py
@@ -29,10 +29,8 @@ class Builder:
     if not os.path.isdir(self.objdir):
       os.system(f"mkdir {self.objdir}")
 
-    for i in glob.glob(f"{self.srcdir}/**/*.{self.extension}",recursive=True):
-      self.clone_source(i)
-
-    self.get_sources()
+    self.src_files = glob.glob(f"{self.srcdir}/**/*.{self.extension}",recursive=True)
+    self.objects = [change_ext(self.to_dest_name(i), "o") for i in self.src_files]
 
     if with_multi_thread:
       with ThreadPoolExecutor() as exe:
@@ -40,7 +38,6 @@ class Builder:
     else:
       for i in self.src_files:
         if self.compile(i) != 0:
-          os.system(f"rm {self.objdir}/*.cpp")
           return 1
   
     if self.updated or not os.path.exists(self.output):
@@ -48,12 +45,6 @@ class Builder:
       os.system(f"{self.compiler} -fuse-ld=/usr/bin/mold {self.linker_flags} {' '.join(self.objects)} -o {self.output}")
     else:
       print("Already up to date.")
-
-    os.system(f"rm {self.objdir}/*.cpp")
-
-  def get_sources(self):
-    self.src_files = glob.glob(f"{self.objdir}/*.{self.extension}")
-    self.objects = [change_ext(i, "o") for i in self.src_files]
 
   def extract_depend_files(self, text):
     incl = self.include
@@ -72,35 +63,35 @@ class Builder:
     
     return ret
 
+  def to_dest_name(self, file):
+    return self.objdir + "/" + file[len(self.srcdir) + 1:].replace("/", "_")
+
   def compile(self, file):
     if self.check_build(file):
       self.updated = True
-      print(self.actual_src_file_cache[file[file.find("/") + 1:]])
-      return os.system(f"{self.compiler} {self.flags} -c -o {change_ext(file, 'o')} {file}")
+      print(file)
+      return os.system(f"{self.compiler} {self.flags} -MMD -MP -MF {change_ext(self.to_dest_name(file), 'd')} -c -o {change_ext(self.to_dest_name(file), 'o')} {file}")
     
     return 0
 
   def check_build(self, file):
-    ft = os.path.getmtime(self.actual_src_file_cache[file[file.find("/")+1:]])
-    obj_path = change_ext(file, "o")
+    ft = os.path.getmtime(file)
+    obj_path = change_ext(self.to_dest_name(file), "o")
+    dep_path = change_ext(obj_path, "d")
     obj = os.path.getmtime(obj_path) if os.path.exists(obj_path) else 0
 
-    if not os.path.exists(change_ext(file, "o")) or ft > obj:
+    if not os.path.exists(obj_path) or ft > obj:
+      return True
+    
+    if not os.path.exists(change_ext(obj_path, "d")):
       return True
 
-    with open(change_ext(file, "d")) as fst:
+    with open(dep_path) as fst:
       for dep in self.extract_depend_files("".join(fst.readlines())):
         if os.path.getmtime(dep) > obj:
           return True
 
     return False
-
-  def clone_source(self, file):
-    dest = file[len(self.srcdir) + 1:].replace("/","_")
-    self.actual_src_file_cache[dest] = file
-
-    os.system(f"cp -f {file} {self.objdir}/{dest}")
-    os.system(f"{self.compiler} {self.flags} -MM -MP -MF {self.objdir}/{change_ext(dest, 'd')} {file}")
 
 def change_ext(file, ext):
   return file[:file.rfind(".")] + "." + ext
